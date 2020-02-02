bd6385ad7844328aa4d5af6a85b95174b4c36192
jaimemarston <jaimemarston@gmail.com>
2020-02-01 20:20:57 -0500

    01.02.2020

---
+++ src/app/_helpers/auth.guard.ts
@@ -0,0 +1,36 @@
+﻿import { Injectable } from '@angular/core';
+import { AuthenticationService } from '../_services/authentication.service';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+// @Injectable({providedIn: 'root'})
+// export class AuthGuard implements CanActivate {
+//     constructor() {
+
+//     }
+
+//     canActivate(): boolean {
+//         return AuthenticationService.isAuthenticated();
+//     }
+// }
+
+
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const currentUser = this.authenticationService.currentUser;
+        if (currentUser) {
+            // authorised so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+        return false;
+    }
+}
